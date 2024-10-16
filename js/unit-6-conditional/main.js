4e243d6e4fac4ea2f390da253c1481077b15df40
thobt <thobui97@outlook.com>
2024-10-16 22:50:03 +0700

    unit 6 switch case

--- js/unit-6-conditional/main.js
+++ js/unit-6-conditional/main.js
@@ -34,3 +34,80 @@ if (choice === "sunny" || temp > 37) {
  * 30 < BMI =< 40 -> in ra "Béo, cần giảm cân"
  * Trên 40 -> in ra "Rất béo, cần giảm cân ngay"
  */
+
+// switch ... case
+
+// switch (thang_can_so_sanh) {
+//   case lua_chon_1:
+// 	  // run this code;
+// 	  // more code.....
+// 		break;
+// 	case lua_chon_2:
+// 		// run this code;
+// 	  // more code.....
+// 		break;
+
+// 	default:
+// 		// run this code
+// 		break;
+// }
+
+var field = "class";
+switch (field) {
+	case "class":
+		console.log("class");
+		break;
+
+	case "car":
+		console.log("car");
+		break;
+
+	case "password":
+		console.log("password");
+		break;
+
+	case "username":
+		console.log("username");
+		break;
+
+	case "phonenumber":
+		console.log("phonenumber");
+		break;
+
+	default:
+		console.log("khong co thang nao thoa man");
+		break;
+}
+
+var month = 1;
+
+switch (month) {
+	case 1:
+	case 2:
+	case 3:
+		console.log("thang 1");
+		break;
+
+	case 4:
+		console.log("thang 4");
+		break;
+
+	default:
+		console.log("khong ong nao thoa man");
+		break;
+}
+
+// ternary operator: (condition) ? run this code : nguoc_lai
+
+var a = 5;
+var b = 10;
+
+// if(a > b) {
+// 	str = "a > b";
+// } else {
+// 	str = "a < b"
+// }
+
+// c1: (a > b) ? str = "a > b" : str = "a < b";
+var str = a > b ? `${a} > ${b}`: `${a} < ${b}`;
+console.log(str);
